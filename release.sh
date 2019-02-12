a91875c8afeb03164476aa400cddf5f5eda8e1da
Benn Mapes <benn@xiroji.com>
2019-02-12 15:59:02 -0800

    Yet Another attempt

--- release.sh
+++ release.sh
@@ -29,6 +29,7 @@ else
   git commit --allow-empty -m "[skip ci] Harvesting release $PACKAGE_VERSION"
   git tag $PACKAGE_VERSION
   git push --tags origin master
+  git fetch origin
   git checkout -b development --track origin/development
   git rebase master
   git push -f origin development
