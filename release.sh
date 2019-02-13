4a6372f7bfa6fe3aaceddbb13042d000980dd9b4
Benn Mapes <benn@xiroji.com>
2019-02-12 17:52:52 -0800

    Hopefully this works

--- release.sh
+++ release.sh
@@ -29,10 +29,7 @@ else
   git commit --allow-empty -m "[skip ci] Harvesting release $PACKAGE_VERSION"
   git tag $PACKAGE_VERSION
   git push --tags origin master
-  git fetch origin
-  git checkout -b development --track origin/development
-  git rebase master
-  git push -f origin development
-  git checkout master
+  git pull --rebase origin development
+  git push origin master:development
   echo "Created Release $PACKAGE_VERSION"
 fi
